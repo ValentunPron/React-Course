f2ebdb79c0356139977c0fe393a9ccea67ca9074
Valentyn <valentunpron@gmail.com>
2022-09-23 15:20:10 +0300

    work with a buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag='h2'>Text</Htag>
+			<Button appearance="primary" arrow="right">Button</Button>
+			<Button appearance="ghost" arrow="right">Button</Button>
 		</>
 	);
 }
