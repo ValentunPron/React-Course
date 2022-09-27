19887476935e9f1ab3f824bf7f37ce271adc7c22
Valentyn <valentunpron@gmail.com>
2022-09-27 09:28:41 +0300

    layout component

---
+++ layout/Layout.props.ts
@@ -0,0 +1,6 @@
+import { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode } from "react";
+
+
+export interface LayoutProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	children: ReactNode;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-import { Button, Htag, P, Tag } from '../components';
+import React, { useState } from "react";
+import { Button, Htag, P, Rating, Tag } from '../components';
+import { Layout } from "../layout/Layout";
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
+
 	return (
-		<>
-			<Htag tag='h2'>Text</Htag>
+		<Layout>
+			<Htag tag='h2'>Testing Program</Htag>
+			<Button appearance="primary" arrow="right">Button</Button>
+			<Button appearance="ghost" arrow="right">Button</Button>
 			<P size="small">Hello World</P>
 			<P>Hello World</P>
 			<P size="medium">Hello World</P>
 			<P size="big">Hello World</P>
-			<Button appearance="primary" arrow="right">Button</Button>
-			<Button appearance="ghost" arrow="right">Button</Button>
 			<Tag size='s'>Ghost</Tag>
 			<Tag size='m' color='red'>Red</Tag>
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Primary</Tag>
-		</>
+			<Rating rating={rating} isEditable={true} setRating={setRating} />
+		</Layout>
 	);
 }
 
